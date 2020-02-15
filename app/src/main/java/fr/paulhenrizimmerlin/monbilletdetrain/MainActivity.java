f47d296111f1498ba2e0daf585a229b7529f37f2
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2020-02-15 19:29:39 +0100

    Initial commit

---
+++ app/src/main/java/fr/paulhenrizimmerlin/monbilletdetrain/MainActivity.java
@@ -0,0 +1,46 @@
+package fr.paulhenrizimmerlin.monbilletdetrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import static androidx.preference.PreferenceManager.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setDefaultValues(this, R.xml.preferences, false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.preferences:
+            {
+                Intent intent = new Intent();
+                intent.setClassName(this, "fr.paulhenrizimmerlin.monbilletdetrain.SettingsActivity");
+                startActivity(intent);
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
