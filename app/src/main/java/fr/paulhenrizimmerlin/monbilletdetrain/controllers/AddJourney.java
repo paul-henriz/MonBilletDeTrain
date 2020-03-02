e6eb16b71fd8cbb4cda80c6218cbbcd17c595774
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2020-03-02 22:26:37 +0100

    Ajout d'un voyage
    Signed-off-by: Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>

---
+++ app/src/main/java/fr/paulhenrizimmerlin/monbilletdetrain/controllers/AddJourney.java
@@ -0,0 +1,16 @@
+package fr.paulhenrizimmerlin.monbilletdetrain.controllers;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.paulhenrizimmerlin.monbilletdetrain.R;
+
+public class AddJourney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_journey);
+    }
+}
