04b37c881283da0082657300c0328619789e27e2
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2020-03-03 10:41:44 +0100

    Ajout des annotation pour Room
    Signed-off-by: Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>

--- app/src/main/java/fr/paulhenrizimmerlin/monbilletdetrain/models/Journey.java
+++ app/src/main/java/fr/paulhenrizimmerlin/monbilletdetrain/models/Journey.java
@@ -1,9 +1,16 @@
 package fr.paulhenrizimmerlin.monbilletdetrain.models;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import java.util.Calendar;
 import java.util.Date;
 
+@Entity(tableName = "journey_table")
 public class Journey {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
     String departure;
     String arrival;
     Date date;
